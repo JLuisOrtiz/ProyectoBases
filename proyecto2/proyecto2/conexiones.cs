19982846c7ed019e034a26a6629f1da37cd6bb5f
JLuisOrtiz <jslsortiz428@gmail.com>
2016-11-03 20:01:23 -0600

    Se modifico en el diseño

--- proyecto2/proyecto2/conexiones.cs
+++ proyecto2/proyecto2/conexiones.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
 
 namespace proyecto2
 {
     class conexiones
     {
-
+      
     }
 }
